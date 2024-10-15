847735da6242095afeada2969d0641767469562f
Simon H-M <simonhm31@gmail.com>
2024-10-14 20:26:53 -0500

    copied everything from branch - unable to merge without write access

--- check_whitespace_test.cpp
+++ check_whitespace_test.cpp
@@ -2,30 +2,34 @@
 
 #include "check_whitespace.h"
 
-void validate_trim(const char* source, const char* anticipated) {
-    char* outcome = trim(source);
-    ASSERT_STREQ(anticipated, outcome);
-    free(outcome);
+void assert_stripped_output(const char* original_input, const char* expected_output) {
+    char* stripped_result = strip(original_input);
+    if (stripped_result != NULL) {
+        ASSERT_STREQ(expected_output, stripped_result);
+    } else {
+    }
+    free(stripped_result);
 }
 
-TEST(strip, EmptyString) {
-    ASSERT_STREQ("", strip(""));
+
+TEST(assert_stripped_output, EmptyString) {
+    ("", "");
 }
 
 TEST(strip, NoWhitespace) {
-    ASSERT_STREQ("frog", strip("frog"));
+    assert_stripped_output("frog", "frog");
 }
 
 TEST(strip, WhitespaceOnFront) {
-    ASSERT_STREQ("frog", strip("   frog"));
+    assert_stripped_output("   frog", "frog");
 }
 
 TEST(strip, WhitespaceOnBack) {
-    ASSERT_STREQ("frog", strip("frog  "));
+    assert_stripped_output("frog  ", "frog");
 }
 
 TEST(strip, WhitespaceOnBothEnds) {
-    ASSERT_STREQ("frog", strip("  frog     "));
+    assert_stripped_output("  frog     ", "frog");
 }
 
 TEST(is_clean, EmptyString) {
